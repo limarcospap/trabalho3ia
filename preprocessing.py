247695ace84fe136c83709b83ec0213d0366e9d9
Marcos Paulo <56302570+limarcospap@users.noreply.github.com>
2020-11-11 14:55:45 -0300

    Create preprocessing.py

---
+++ preprocessing.py
@@ -0,0 +1,13 @@
+import numpy as np
+import pandas as pd
+
+#Attibutes that will be used:
+
+columns = [
+    'Crossing', 'Finishing','HeadingAccuracy', 'ShortPassing', 'Volleys', 'Dribbling', 'Curve', 'FKAccuracy', 'LongPassing', 'BallControl', 'Acceleration', 'SprintSpeed', 'Agility', 'Reactions', 'Balance', 'ShotPower', 'Jumping', 'Stamina', 'Strength', 'LongShots', 'Aggression', 'Interceptions', 'Positioning', 'Vision', 'Penalties', 'Composure', 'Marking', 'StandingTackle', 'SlidingTackle', 'GKDiving', 'GKHandling', 'GKKicking', 'GKPositioning', 'GKReflexes','Weak Foot','Work Rate', 'Position'
+]
+
+#Opening the dataset
+data = pd.read_csv('data.csv'
+           , usecols = columns
+           )
